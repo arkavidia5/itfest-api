d54ab89be4e59b4c4f878649256362c00a068d0d
Nella Zabrina <nellazabrinapramata@yahoo.co.id>
2019-01-20 01:33:01 +0700

    add js for button in tenant, item, transaction

---
+++ views/dashboard.js
@@ -0,0 +1,26 @@
+$('document').ready(function() {
+    $('#new-button').click(function() {
+        // $('#new-button').remove();
+        if ($('#tenant').length == 1) {
+            $("#table-tenant").append('<tr class="new-record"><td><input type="text" class="add-txtbox" value="" /></td><td><input type="text" class="add-txtbox" value="" /></td></tr>');
+        }
+        else if ($('#item').length == 1) {
+            $("#table-item").append('<tr class="new-record"><td><input type="text" class="add-txtbox" value="" /></td><td><input type="text" class="add-txtbox" value="" /></td><td><input type="text" class="add-txtbox" value="" /></td></tr>');
+        }
+        else if ($('#transaction').length == 1) {
+            $("#table-transaction").append('<tr class="new-record"><td><input type="text" class="add-txtbox" value="" /></td><td><input type="text" class="add-txtbox" value="" /></td></tr>');
+        }
+
+
+        if ($('#save-button').length == 0 && $('#cancel-button').length == 0) {
+            $('.button').append('<button id="save-button" class="add-button">Save</button> <button id="cancel-button" class="add-button">Cancel</button>');
+
+            $('#cancel-button').click(function() {
+                $('.new-record').remove();
+                $('.cancel-button').remove();
+                $('.save-button').remove();
+            });
+        }
+    });
+
+});
